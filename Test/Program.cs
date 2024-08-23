deea562e31a479d43f3005c68feb4b55ec36d099
Stephan <kochurov05@mail.ru>
2024-08-23 20:49:49 +0400

    Добавьте файлы проекта.

---
+++ Test/Program.cs
@@ -0,0 +1,37 @@
+﻿
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            Console.WriteLine(i);
+        }
+        Person tom = new("Tom", "Smith");
+
+        Console.WriteLine(tom.lastname);    // Tom Smith
+
+    }
+
+    
+}
+
+
+class Person
+{
+    string firstName;
+    string lastName;
+    //public string Name
+    //{
+    //    get { return $"{firstName} {lastName}"; }
+
+    //    set => firstName = value;
+    //}
+
+
+    public Person(string firstName, string lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
